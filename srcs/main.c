d76aa1894c34052d02791e8858f3ef8715b5dbe9
Anouar Saadi <asaadi@e2r4p5.1337.ma>
2021-01-02 18:04:09 +0100

    env_export_unset_done

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,11 @@
 /*   By: asaadi <asaadi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/31 08:54:21 by asaadi            #+#    #+#             */
-/*   Updated: 2021/01/02 10:10:43 by asaadi           ###   ########.fr       */
+/*   Updated: 2021/01/02 17:08:47 by asaadi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-
 
 int 	count_vars_env(char **env_list)
 {
@@ -26,105 +24,98 @@ int 	count_vars_env(char **env_list)
 	return (count);
 }
 
-// char		**unset_env(char *l, char **envp)
-// {
-// 	char **p;
-// 	char **ret;
-// 	char **split;
-// 	int	len;
-// 	int i;
-
-// 	p = envp;
-// 	len = count_line(p);
-// 	if (!(ret = (char **)malloc(sizeof(char *) * (len - 1))))
-// 		return (NULL);
-// 	i = 0;
-// 	while (i < len)
-// 	{
-// 		split = ft_split(p[i], '=');
-// 		if (ft_strncmp(split[0], l, ft_strlen(l)) != 0)
-// 		{
-// 			ret[i] = p[i];
-// 		}
-// 		i++;
-// 		ft_free_split(split);
-// 	}
-// 	return (ret);
-// }
-
-// char		**export_env(char *l, char **envp)
-// {
-// 	char **p;
-// 	int len;
-
-// 	p = envp;
-// 	len = count_line(p);
-// 	p[len] = l;
-// 	p[len + 1] = NULL;
-// 	return (p);
-// }
-
-
-char	*check_var_env(char **envp, char *var_to_check)
+void	ft_free_split(char **split)
 {
-	int	count_vars;
-	char *val_to_ret;
-	char **equ_split;
-	char *tmp;
+	int i;
 
-	count_vars = 0;
-	val_to_ret = ft_strdup("");
-	while (envp[count_vars])
+	i = 0;
+	while (split[i])
 	{
-		equ_split = ft_split(envp[count_vars], '=');
-		if (ft_strncmp(equ_split[0], var_to_check, ft_strlen(equ_split[0])) == 0)
-		{
-			tmp = ft_strchr(envp[count_vars], '=');
-			val_to_ret = ft_strdup(tmp + 1);
-		}
-		ft_free_split(equ_split);
-		count_vars++;
+		free(split[i]);
+		i++;
 	}
-	return (val_to_ret);
+	free(split);
+}
+
+char	**unset_function(char **envp, char *var_to_remove)
+{
+	char **new_envp;
+	char **equ_split;
+	int	len;
+	int i;
+	int j;
+
+	len = count_vars_env(envp);
+	i = 0;
+	j = 0;
+	if (!(new_envp = (char **)malloc(sizeof(char*) * len - 1)))
+		return (NULL);
+	while (envp[i])
+	{
+		equ_split = ft_split(envp[i], '=');
+		if (strncmp(equ_split[0], var_to_remove, ft_strlen(equ_split[0])) != 0)
+			new_envp[j++] = envp[i];
+		i++;
+		ft_free_split(equ_split);
+	}
+	new_envp[j] = NULL;
+	return (new_envp);
+}
+
+void	env_function(char **envp)
+{
+	int i;
+
+	i = 0;
+	while (envp[i])
+	{
+		ft_putendl_fd(envp[i], 1);
+		i++;
+	}
+}
+
+char	**export_function(char **envp, char *var_to_add)
+{
+	char **new_envp;
+	int len;
+	int i;
+	int j;
+
+	len  = count_vars_env(envp);
+	if (!(new_envp = (char **)malloc(sizeof(char*) * len + 1)))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (envp[i])
+	{
+		if (strncmp(envp[i], "_", 1) == 0)
+		{
+			new_envp[j++] = var_to_add;
+			new_envp[j++] = envp[i];
+
+		}
+		else
+			new_envp[j++] = envp[i];
+		i++;
+	}
+	new_envp[j] = NULL;
+	return (new_envp);
 }
 
 int		main(int ac, char **av, char **envp)
 {
 	
-	// int i = 0;
-	// int len = 0;
+	int i;
+
+	i = 0;
 	ac = 0;
 	av[1] = "hhhhh";
-	// char **p;
-	// // p = envp;
-	// // ft_putnbr_fd(len, 1);
-	// // write(1, "\n", 1);
-	// p = unset_env("mia",envp);
-	// len = count_line(p);
-	// while (i < len)
-	// {
-	// 	puts(p[i]);
-	// 	i++;
-	// }
-	// char *str = ft_strchr("t=lllll", '=');
-	// ft_putendl_fd(str + 1, 1);
-	char *str = check_var_env(envp, "PWD");
-	ft_putendl_fd(str, 1);
+	env_function(envp);
+	write(1, "+++========+++\n", 15);
+	envp = export_function(envp, "myvar=100");
+	env_function(envp);
+	write(1, "+++========+++\n", 15);
+	envp = unset_function(envp, "myvar");
+	env_function(envp);
 	return (0);
 }
-
-// int main()
-// {
-// 	char *str;
-// 	char **srch;
-
-// 	str = "mayvar=300";
-// 	srch = ft_split(str, '=');
-// 	int i = 0;
-// 	while(i < 2)
-// 	{
-// 		printf("%s ==> %s\n", str, srch[i]);
-// 		i++;
-// 	}
-// 	return (0);
-// }

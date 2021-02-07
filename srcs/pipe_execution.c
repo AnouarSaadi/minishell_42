b2d122ecc7f4e07e5c13eb2c2dbff73fcdb7513a
Anouar Saadi <asaadi@e2r3p11.1337.ma>
2021-02-07 13:01:53 +0100

    Minishell: pipe: cat | cat | ls working: save and restore stdin stdout

--- srcs/pipe_execution.c
+++ srcs/pipe_execution.c
@@ -6,7 +6,7 @@
 /*   By: asaadi <asaadi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 14:24:33 by asaadi            #+#    #+#             */
-/*   Updated: 2021/02/06 15:32:07 by asaadi           ###   ########.fr       */
+/*   Updated: 2021/02/07 13:00:28 by asaadi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,25 @@
 
 void pipe_execution(t_list *pipe_cmd_list, char **envp, t_exec *exec)
 {
-    int fd[2];
+    int *fd = malloc(sizeof(int) * 2);
     int tmp_fd;
     int tmp_out;
     t_cmd *tmp__cmd;
     pid_t c_pid;
     int size;
-    int status;
+    // int status;
     int i;
     int j = 0;
 
-    // Save stdin and stdout
-    tmp_fd = dup(0); // Origin tmp_fd = 0
-    tmp_out = dup(1);
     size = ft_lstsize(pipe_cmd_list);
     int *pid = malloc(sizeof(int) * (size + 1));
-    //save fds
     while (pipe_cmd_list)
     {
-        // if (pipe_cmd_list->next)
-        pipe(fd); // check failure of pipe
+        // Save stdin and stdout
+        tmp_fd = dup(0); // Origin tmp_fd = 0
+        tmp_out = dup(1);
+        if (pipe_cmd_list->next)
+            pipe(fd); // check failure of pipe
         printf("|fd[0] = %d fd[1] == %d|\n", fd[0], fd[1]);
         c_pid = fork();
         if (c_pid == 0)
@@ -45,7 +44,8 @@ void pipe_execution(t_list *pipe_cmd_list, char **envp, t_exec *exec)
             if (pipe_cmd_list->next)
                 if (dup2(fd[1], 1) == -1) // check failure of pipe
                     perror("dup2");
-            close(fd[0]);
+            if (close(fd[0]) == -1)
+                perror("Close fd[0]");
             // if(tmp__cmd->redir_list)
             // 	redir_is_in_cmd(exec, tmp__cmd, &envp);
             if (check_if_built_in(exec->args[0]))
@@ -63,14 +63,15 @@ void pipe_execution(t_list *pipe_cmd_list, char **envp, t_exec *exec)
         }
         else if (c_pid < 0)
         {
-            // waitpid(c_pid, &status, 0);
-            // kill(c_pid, SIGPIPE);
+            perror("fork");
             exit_function(1);
         }
         else
         {
-            close(fd[0]);
-            close(fd[1]);
+           if (close(fd[0]) == -1)
+            perror("2 - Close fd[0]");
+            if (close(fd[1]) == -1)
+                perror("Close fd[1]");
             tmp_fd = fd[0];
             // close(tmp_out);
         }
@@ -85,14 +86,12 @@ void pipe_execution(t_list *pipe_cmd_list, char **envp, t_exec *exec)
     dup2(tmp_fd, 0); // ADD to Code
     dup2(tmp_out, 1);
     i = 0;
-    if (!size)
+    printf("%d\n", size);
+    while (i < size)
     {
-        waitpid(c_pid, &status, 0);
-        kill(c_pid, SIGPIPE);
-    }
-    while (i < size + 1)
-    {
+        // wait(0);
         waitpid(pid[i], &status, 0);
+        kill(pid[i], SIGPIPE);
         i++;
     }
 }

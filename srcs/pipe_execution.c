f286eb1fb3cdccd11c29be94bec5b4b76d85698e
Anouar Saadi <asaadi@e2r2p3.1337.ma>
2021-02-16 12:53:35 +0100

    Minishell: Pipes && Redirections: Done

--- srcs/pipe_execution.c
+++ srcs/pipe_execution.c
@@ -6,7 +6,7 @@
 /*   By: asaadi <asaadi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 14:24:33 by asaadi            #+#    #+#             */
-/*   Updated: 2021/02/13 14:46:05 by asaadi           ###   ########.fr       */
+/*   Updated: 2021/02/16 12:52:21 by asaadi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 ** The exit status of the pipeline is the exit status of command2.
 */
 
-void        check_for_failed(char *strer)
+void check_for_failed(char *strer)
 {
     //free data and exit
     //TODO: check leak memory
@@ -27,93 +27,59 @@ void        check_for_failed(char *strer)
     exit_function(1);
 }
 
-// void  ft_waitpid_s(t_exec *exec, int size, int signal)
-// {
-//     int i;
-
-//     i = 0;
-//     while (i < size)
-//     {
-//         waitpid(exec->pid_s[i], &exec->status, 0);
-//         kill(exec->pid_s[i], signal);
-//         i++;
-//     }
-//     ft_free_arr((void**)&(exec->pid_s));
-// }
-
-// int get_fd_input(t_cmd *cmd_list, int *fd_in)
-// {
-//     t_list *tmp_red;
-//     int red_is_in;
-    
-//     // fd_name = ft_strdup("");
-//     red_is_in = 0;
-//     if (cmd_list->redir_list)
-//     {
-//         tmp_red = cmd_list->redir_list;
-//         while(tmp_red)
-//         {
-//             if (((t_redir*)tmp_red->content)->type == e_state_gt)
-//             {
-//                 *fd_in = redirect_to_std_out(((t_redir*)tmp_red->content)->file);
-//                 red_is_in = 1;
-//             }
-//         }
-//     }
-//     return(red_is_in);
-// }
-
-
 void pipe_execution(t_list *pipe_cmd_list, t_exec *exec)
 {
-    int tmp_in;
-    int tmp_out;
-    t_cmd   *tmp_cmd;
+    int save_fds[2];
+    t_cmd *tmp_cmd;
     int pipe_fd[2];
     int size;
-    int fd_in;
-    int fd_out;
+    int fds[2];
     int i;
 
     (void)exec;
 
-    tmp_in = dup(0);// Save in/out
-    tmp_out = dup(1);
+    save_fds[0] = dup(0); // Save in/out
+    save_fds[1] = dup(1);
     size = ft_lstsize(pipe_cmd_list);
-    exec->pid_s = malloc(sizeof(pid_t) * (size + 1));
-    // if (get_fd_input())
-    fd_in = dup(tmp_in);//default in if there's no redirection to stdin
+    if(!(exec->pid_s = malloc(sizeof(pid_t) * (size + 1))))
+        ft_putendl_fd("Allocation Error", 2);
+    fds[0] = dup(save_fds[0]); //default in if there's no redirection to stdin
     i = 0;
     while (pipe_cmd_list)
     {
         //redirect input
-        dup2(fd_in, 0);
-        close(fd_in);
+        dup2(fds[0], 0);
+        close(fds[0]);
         if (pipe_cmd_list->next == NULL)
-            fd_out = dup(tmp_out);//Default out if there's no redirection to stdout
+            fds[1] = dup(save_fds[1]); //Default out if there's no redirection to stdout
         else
         {
             pipe(pipe_fd);
-            fd_out = pipe_fd[1];
-            fd_in = pipe_fd[0];
-            // close(pipe_fd[0]);
-            // close(pipe_fd[1]);
+            fds[1] = pipe_fd[1];
+            fds[0] = pipe_fd[0];
         }
-        dup2(fd_out, 1);
-        close(fd_out);
+        dup2(fds[1], 1);
+        close(fds[1]);
         exec->c_pid = fork();
         if (exec->c_pid == 0)
         {
-            tmp_cmd = (t_cmd*)pipe_cmd_list->content;
-            fill_args(tmp_cmd->word_list, exec);
-             if (check_if_built_in(exec->args[0]))
-                built_ins_execution(exec);
+            close(pipe_fd[0]);
+            tmp_cmd = (t_cmd *)pipe_cmd_list->content;
+            if (tmp_cmd->redir_list)
+                redir_is_in_cmd(exec, tmp_cmd);
             else
             {
-                if (get_cmd_path(exec))
-                    if (execve(exec->args[0], exec->args, exec->envp) == -1)
-                        check_for_failed(strerror(errno));
+                fill_args(tmp_cmd->word_list, exec);
+                if (check_if_built_in(exec->args[0]))
+                    built_ins_execution(exec);
+                else
+                {
+                    if (get_cmd_path(exec))
+                        if (execve(exec->args[0], exec->args, exec->envp) == -1)
+                            check_for_failed(strerror(errno));
+                }
             }
+
             exit_function(1);
         }
         else if (exec->c_pid == -1)
@@ -122,125 +88,16 @@ void pipe_execution(t_list *pipe_cmd_list, t_exec *exec)
         pipe_cmd_list = pipe_cmd_list->next;
     }
     exec->pid_s[i] = 0;
-    dup2(tmp_in, 0);
-    dup2(tmp_out, 1);
-    close(tmp_in);
-    close(tmp_out);
+    dup2(save_fds[0], 0);
+    dup2(save_fds[1], 1);
+    close(save_fds[0]);
+    close(save_fds[1]);
     close(pipe_fd[0]);
     close(pipe_fd[1]);
     i = 0;
-    // while(wait(0) > 0);
-    while(i < size)
+    while (i < size)
     {
         waitpid(exec->pid_s[i], &exec->status, 0);
         i++;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// void pipe_execution(t_list *pipe_cmd_list, char **envp, t_exec *exec)
-// {
-//     int fd[2];
-//     int tmp_fd;
-//     int tmp_out;
-//     t_cmd *tmp__cmd;
-//     int size;
-//     int j = 0;
-
-//     size = ft_lstsize(pipe_cmd_list);
-//     exec->pid_s = malloc(sizeof(int) * (size + 1));
-//     tmp_fd = dup(0);
-//     tmp_out = dup(1);
-//     while (pipe_cmd_list)
-//     {
-//         if (pipe_cmd_list->next)
-//             if (pipe(fd) == -1)
-//                 check_for_failed(strerror(errno));
-//         exec->c_pid = fork();
-//         if (exec->c_pid == 0)
-//         {
-//             tmp__cmd = (t_cmd *)pipe_cmd_list->content;
-//             fill_args(tmp__cmd->word_list, exec);
-//             if (dup2(tmp_fd, 0) == -1)
-//                 check_for_failed(strerror(errno));
-//             if (pipe_cmd_list->next)
-//                 if (dup2(fd[1], 1) == -1)
-//                     check_for_failed(strerror(errno));
-//             if (close(fd[0]) == -1)
-//                 check_for_failed(strerror(errno));
-//             if (check_if_built_in(exec->args[0]))
-//                 built_ins_execution(exec, &envp);
-//             else
-//             {
-//                 if (get_cmd_path(exec->args, envp))
-//                     if (execve(exec->args[0], exec->args, envp) < 0)
-//                         check_for_failed(strerror(errno));
-//             }
-//             exit_function(0);
-//         }
-//         else if (exec->c_pid == -1)
-//             check_for_failed(strerror(errno));
-//         else
-//         {
-//         //    if (close(fd[0]) == -1)
-//         //         perror("close_fd_0");//check_for_failed(strerror(errno));
-//             // if (close(fd[1]) == -1)
-//                 // perror("close_fd_1");//check_for_failed(strerror(errno));
-//             // tmp_fd = fd[0];
-//         }
-//         exec->pid_s[j++] = exec->c_pid;
-//         pipe_cmd_list = pipe_cmd_list->next;
-//     }
-//     exec->pid_s[j] = 0;
-//     // close(fd[0]);
-//     // close(fd[1]);
-//     dup2(tmp_fd, 0);
-//     dup2(tmp_out, 1);
-//     close(tmp_fd);
-//     close(tmp_out);
-//     ft_waitpid_s(exec, size, SIGPIPE);
-// }
-
-// cat | ls | cat
--- srcs/redirections.c
+++ srcs/redirections.c
@@ -6,16 +6,18 @@
 /*   By: asaadi <asaadi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 12:04:12 by asaadi            #+#    #+#             */
-/*   Updated: 2021/02/15 16:40:59 by asaadi           ###   ########.fr       */
+/*   Updated: 2021/02/16 11:14:08 by asaadi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// void        check_failed()
-// {
-//     ft_putendl_fd()
-// }
+void dup2_fails(int err)
+{
+    ft_putstr_fd("bash: dup2: ", 2);
+    ft_putendl_fd(strerror(err), 2);
+    exit_function(1);
+}
 
 void redir_is_in_cmd(t_exec *exec, t_cmd *cmd)
 {
@@ -39,11 +41,7 @@ void redir_is_in_cmd(t_exec *exec, t_cmd *cmd)
                 exit(1);
             }
             if (dup2(fds[1], 1) == -1)
-            {
-                ft_putstr_fd("bash: dup2: ", 2);
-                ft_putendl_fd(strerror(errno), 2);
-                exit_function(1);
-            }
+                dup2_fails(errno);
             close(fds[1]);
         }
         else if (((t_redir *)tmp__redir->content)->type == e_state_dgt)
@@ -57,11 +55,7 @@ void redir_is_in_cmd(t_exec *exec, t_cmd *cmd)
                 exit(1);
             }
             if (dup2(fds[1], 1) == -1)
-            {
-                ft_putstr_fd("bash: dup2: ", 2);
-                ft_putendl_fd(strerror(errno), 2);
-                exit_function(1);
-            }
+                dup2_fails(errno);
             close(fds[1]);
         }
         else if (((t_redir *)tmp__redir->content)->type == e_state_lt)
@@ -75,11 +69,7 @@ void redir_is_in_cmd(t_exec *exec, t_cmd *cmd)
                 exit(1);
             }
             if (dup2(fds[0], 0) == -1)
-            {
-                ft_putstr_fd("bash: dup2: ", 2);
-                ft_putendl_fd(strerror(errno), 2);
-                exit_function(1);
-            }
+                dup2_fails(errno);
             close(fds[0]);
         }
         tmp__redir = tmp__redir->next;
@@ -93,15 +83,9 @@ void redir_is_in_cmd(t_exec *exec, t_cmd *cmd)
             exec_cmd(exec);
     }
     if (dup2(save_fds[0], 0) == -1)
-    {
-        ft_putstr_fd("bash: dup2: ", 2);
-        ft_putendl_fd(strerror(errno), 2);
-        exit_function(1);
-    }
+        dup2_fails(errno);
+    close(save_fds[0]);
     if (dup2(save_fds[1], 1) == -1)
-    {
-        ft_putstr_fd("bash: dup2: ", 2);
-        ft_putendl_fd(strerror(errno), 2);
-        exit_function(1);
-    }
+        dup2_fails(errno);
+    close(save_fds[0]);
 }
